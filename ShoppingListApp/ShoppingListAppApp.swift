f0df665ce251eab35933e37f195065bb7080b694
Bartosz Kordek <bartosz.kordek10@gmail.com>
2022-01-06 16:21:01 +0100

    Initial Commit

---
+++ ShoppingListApp/ShoppingListAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShoppingListAppApp.swift
+//  ShoppingListApp
+//
+//  Created by Bartosz Kordek on 06/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct ShoppingListAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
