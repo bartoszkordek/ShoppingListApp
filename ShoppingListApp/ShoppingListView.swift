c0af5e7b896f2fe3c7c8b2fbb4bf280eceed2443
Bartosz Kordek <bartosz.kordek10@gmail.com>
2022-01-13 01:23:39 +0100

    enter product name and units input labels

--- ShoppingListApp/ShoppingListView.swift
+++ ShoppingListApp/ShoppingListView.swift
@@ -6,29 +6,51 @@
 //
 
 import SwiftUI
+import Combine
 
 struct ShoppingListView: View {
-    @State var shoppingList = ShoppingList();
+    @State private var shoppingList = ShoppingList();
+    @State private var addedProductName: String = "";
+    @State private var addedProductUnits: String = "0";
     
     var body: some View {
         NavigationView {
-            List {
-                ForEach(shoppingList.getShoppingList(), id: \.self) { product in
-                    Text(product.getName())
+            VStack{
+                TextField("Enter product name", text: $addedProductName)
+                    .padding();
+                TextField("Units", text: $addedProductUnits)
+                    .keyboardType(.numberPad)
+                    .onReceive(Just(addedProductUnits)) { newValue in
+                        let filtered = newValue.filter { "0123456789".contains($0) }
+                        if filtered != newValue {
+                            self.addedProductUnits = filtered
+                        }
+                    }
+                    .padding();
+                
+                List {
+                    ForEach(shoppingList.getShoppingList(), id: \.self) { product in
+                        HStack{
+                            Text(product.getName());
+                            Text(String(product.getUnits()));
+                        }
+                    }
                 }
+                .navigationBarTitle(Text("Products"))
+                
+                .navigationBarItems(trailing: Button(action: {
+                    self.addRow(name: addedProductName, units: addedProductUnits);
+                }) {
+                    Image(systemName: "plus")
+                })
             }
-            .navigationBarTitle(Text("Products"))
             
-            .navigationBarItems(trailing: Button(action: {
-                self.addRow()
-            }) {
-                Image(systemName: "plus")
-            })
         }
     }
     
-    private func addRow() {
-        self.shoppingList.addProduct(product: Product(name: "Banana", units: 9));
+    private func addRow(name: String, units: String) {
+        if(isProductNameEmpty(productName: name)) {return;}
+        self.shoppingList.addProduct(product: Product(name: name, units: (Int(units) ?? 0)));
         }
 }
 
